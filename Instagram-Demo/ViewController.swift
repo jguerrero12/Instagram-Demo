a858f552a4f8455c4724ca476e78f0caceb24539
Jose A. Guerrero <jose3292@gmail.com>
2017-03-06 01:09:07 -0500

    Initial Commit

---
+++ Instagram-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram-Demo
+//
+//  Created by Jose Guerrero on 3/6/17.
+//  Copyright © 2017 Jose Guerrero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
